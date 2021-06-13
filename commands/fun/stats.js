32cbbd6694b4095f9a599f7583628cbfa41577ef
Galaxy Gaming <76540040+galaxyff@users.noreply.github.com>
2021-06-13 07:43:56 +0530

    Update stats.js

--- commands/fun/stats.js
+++ commands/fun/stats.js
@@ -4,7 +4,7 @@ const prettyMs = require('pretty-ms');
 module.exports.run = async (client, message, args) => {
 	const msg = await message.reply('Loading...').then(m => m.delete(), 1000);
 	const embed = new Discord.MessageEmbed()
-		.setAuthor('DisCruft bot stats')
+		.setAuthor('Disney Dev bot stats')
 		.addField('__Cache__\n', `**Guilds**: ${client.guilds.cache.size} \n\n`, true)
 		.addField('__Uptime__\n', `**Process**: ${prettyMs(process.uptime() * 1000)}`, true)
 		.addField('__Movement__\n', `**API Latency**: ${Math.round(client.ws.ping)}\n**Bot Latency**: ${msg.createdTimestamp - message.createdTimestamp}ms`, true);
@@ -12,6 +12,6 @@ module.exports.run = async (client, message, args) => {
 };
 module.exports.config = {
 	name: 'stats',
-	aliases: [],
+	aliases: ["info", "stats"],
 	description: 'Shows the stats of the bot!',
 };
